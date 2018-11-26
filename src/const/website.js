19270e5c395125ac413c3a603cfac7288ea3f2ff
Administrator <Administrator@192.168.1.108>
2018-11-26 17:58:55 +0800

    ui项目初创

---
+++ src/const/website.js
@@ -0,0 +1,43 @@
+/*
+ *    Copyright (c) 2018-2025, clour All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the clour.org developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: clour (slorys@hotmail.com)
+ */
+
+export default {
+  title: 'Clour',
+  logo: 'Cor',
+  author: '经济信息平台',
+  whiteList: ['/login', '/404', '/401', '/lock'],
+  info: {
+    title: '新北区经济社会发展信息平台',
+    list: [
+      '是一个基于Spring Cloud、oAuth2.0开发基于Vue前后分离的开发平台',
+      ' 是一个基于vue+vuex+vue-router快速后台管理系统，采用token交互验证方式。',
+      '最大程度上帮助企业节省时间成本和费用开支。 ',
+      'QQ群：1922454690',
+      '当前版本：v1.3.0'
+    ]
+  },
+  wel: {
+    title: '新北区经济社会发展信息平台',
+    list: [
+      '是一个基于Spring Cloud、oAuth2.0开发基于Vue前后分离的开发平台',
+      ' 是一个基于vue+vuex+vue-router快速后台管理系统，采用token交互验证方式。',
+      '最大程度上帮助企业节省时间成本和费用开支。 ',
+      'QQ群：1922454690',
+      '当前版本：v1.3.0'
+    ]
+  }
+}
